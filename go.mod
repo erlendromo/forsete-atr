c9818c6ee24e9eaec094586ee48faf2dbc76dca3
erlend <erlenrom@stud.ntnu.no>
2025-03-03 10:50:14 +0100

    feat: Better environment management, removed dependency #28

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/erlendromo/forsete-atr
 
 go 1.23.4
 
-require github.com/joho/godotenv v1.5.1
+require gopkg.in/yaml.v3 v3.0.1
--- main.go
+++ main.go
@@ -2,14 +2,7 @@ package main
 
 import (
 	"github.com/erlendromo/forsete-atr/src/cmd/rest"
-	"github.com/joho/godotenv"
 )
-
-func init() {
-	if err := godotenv.Load(".env"); err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	rest.StartRestService()
