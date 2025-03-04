44bf8dbc0d5e948195363fc4e04eb3a47e3173c8
erlend <erlenrom@stud.ntnu.no>
2025-03-04 15:34:27 +0100

    feat: Set default API_PORT #28

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/erlendromo/forsete-atr/src/cmd/rest"
+	"github.com/erlendromo/forsete-atr/src/util"
 )
+
+func init() {
+	if _, found := os.LookupEnv("API_PORT"); !found {
+		os.Setenv("API_PORT", util.DEFAULT_API_PORT)
+	}
+}
 
 func main() {
 	rest.StartRestService()
