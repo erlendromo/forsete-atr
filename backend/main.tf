28d02fc045daa1018c9c9d98a5db55955c549b8c
erlend <erlenrom@stud.ntnu.no>
2025-03-18 10:22:21 +0100

    backend: Added sample terraform infrastructure for OpenStack #41

---
+++ backend/main.tf
@@ -0,0 +1,82 @@
+# Create Network
+resource "openstack_networking_network_v2" "private_net" {
+  name           = "private_network"
+  admin_state_up = true
+}
+
+# Create Subnet
+resource "openstack_networking_subnet_v2" "private_subnet" {
+  name        = "private_subnet"
+  network_id  = openstack_networking_network_v2.private_net.id
+  cidr        = "192.168.1.0/24"
+  ip_version  = 4
+  enable_dhcp = true
+}
+
+# Create Router
+resource "openstack_networking_router_v2" "router" {
+  name                = "router"
+  external_network_id = "public_network_id"
+}
+
+# Router Interface
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.private_subnet.id
+}
+
+# Security Group
+resource "openstack_networking_secgroup_v2" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic"
+}
+
+# Security Group Rule
+resource "openstack_networking_secgroup_rule_v2" "all_tcp" {
+  security_group_id = openstack_networking_secgroup_v2.allow_all.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+}
+
+# Block Storage
+resource "openstack_blockstorage_volume_v3" "volume" {
+  name        = "my-volume"
+  size        = 10
+  description = "A test volume"
+}
+
+# Compute Instance (VM)
+resource "openstack_compute_instance_v2" "vm" {
+  name            = "my-vm"
+  image_name      = "Ubuntu-22.04"
+  flavor_name     = "m1.small"
+  key_pair        = "my-key"
+  security_groups = [openstack_networking_secgroup_v2.allow_all.name]
+
+  network {
+    uuid = openstack_networking_network_v2.private_net.id
+  }
+
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.volume.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+}
+
+# Floating IP
+resource "openstack_networking_floatingip_v2" "floating_ip" {
+  pool = "public"
+}
+
+
+# Associate Floating IP
+resource "openstack_networking_floatingip_associate_v2" "floating_ip_assoc" {
+  floating_ip = openstack_networking_floatingip_v2.floating_ip.address
+  port_id     = openstack_compute_instance_v2.vm.network.0.uuid
+}
