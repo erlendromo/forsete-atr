0f39c87c5109ac6ee6c5f3d7516ffeb6a0cdc605
erlend <erlenrom@stud.ntnu.no>
2025-03-28 00:19:20 +0100

    Added PostTrOCR, routing and logic for it #27 #31 #37

--- src/api/handler/modelhandler.go
+++ src/api/handler/modelhandler.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"errors"
+	"fmt"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/erlendromo/forsete-atr/src/domain/model"
@@ -66,19 +69,28 @@ func GetRegionSegmentationModels(w http.ResponseWriter, r *http.Request) {
 //	@Failure		500	{object}	util.ErrorResponse
 //	@Router			/forsete-atr/v1/models/region-segmentation-models/ [post]
 func PostRegionSegmentationModel(w http.ResponseWriter, r *http.Request) {
-	modelName := strings.ToLower(r.FormValue("model_name"))
-	if len(modelName) < 3 {
+	// Only accept alphanumeric and dashes
+	modelName := string(
+		regexp.MustCompile(
+			"[^a-zA-Z0-9 -]+",
+		).ReplaceAll(
+			[]byte(strings.ToLower(r.FormValue("model_name"))),
+			[]byte(""),
+		),
+	)
+
+	if len(modelName) < 3 && len(modelName) > 50 {
 		util.ERROR(w, http.StatusBadRequest, errors.New("improper model_name, must be atleast 3 characters long"))
 		return
 	}
 
-	if _, err := os.Open("models/regionsegmentation/model.pt"); err == nil {
+	if _, err := os.ReadDir(fmt.Sprintf("models/regionsegmentation/%s", modelName)); err != nil {
 		util.ERROR(w, http.StatusBadRequest, errors.New("model already exists"))
 		return
 	}
 
-	modelFile, _, err := r.FormFile("model")
-	if err != nil {
+	modelFile, modelHeader, err := r.FormFile("model")
+	if strings.Split(modelHeader.Filename, ".")[1] != "pt" || err != nil {
 		util.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
@@ -119,19 +131,28 @@ func GetLineSegmentationModels(w http.ResponseWriter, r *http.Request) {
 //	@Failure		500	{object}	util.ErrorResponse
 //	@Router			/forsete-atr/v1/models/line-segmentation-models/ [post]
 func PostLineSegmentationModel(w http.ResponseWriter, r *http.Request) {
-	modelName := strings.ToLower(r.FormValue("model_name"))
-	if len(modelName) < 3 {
+	// Only accept alphanumeric and dashes
+	modelName := string(
+		regexp.MustCompile(
+			"[^a-zA-Z0-9 -]+",
+		).ReplaceAll(
+			[]byte(strings.ToLower(r.FormValue("model_name"))),
+			[]byte(""),
+		),
+	)
+
+	if len(modelName) < 3 && len(modelName) > 50 {
 		util.ERROR(w, http.StatusBadRequest, errors.New("improper model_name, must be atleast 3 characters long"))
 		return
 	}
 
-	if _, err := os.Open("models/linesegmentation/model.pt"); err == nil {
+	if _, err := os.ReadDir(fmt.Sprintf("models/linesegmentation/%s", modelName)); err != nil {
 		util.ERROR(w, http.StatusBadRequest, errors.New("model already exists"))
 		return
 	}
 
-	modelFile, _, err := r.FormFile("model")
-	if err != nil {
+	modelFile, modelHeader, err := r.FormFile("model")
+	if strings.Split(modelHeader.Filename, ".")[1] != "pt" || err != nil {
 		util.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
@@ -171,7 +192,7 @@ func GetTextRecognitionModels(w http.ResponseWriter, r *http.Request) {
 //	@Param			config				formData	file	required	"config.json"
 //	@Param			generation_config	formData	file	required	"generation_config.json"
 //	@Param			merges				formData	file	required	"merges.txt"
-//	@Param			preprocessor_config	formData	file	required	"preprocessor_config-json"
+//	@Param			preprocessor_config	formData	file	required	"preprocessor_config.json"
 //	@Param			special_tokens_map	formData	file	required	"special_tokens_map.json"
 //	@Param			tokenizer			formData	file	required	"tokenizer.json"
 //	@Param			tokenizer_config	formData	file	required	"tokenizer_config.json"
@@ -181,19 +202,114 @@ func GetTextRecognitionModels(w http.ResponseWriter, r *http.Request) {
 //	@Failure		404	{object}	util.ErrorResponse
 //	@Router			/forsete-atr/v1/models/text-recognition-models/ [post]
 func PostTextRecognitionModel(w http.ResponseWriter, r *http.Request) {
-	modelName := strings.ToLower(r.FormValue("model_name"))
-	if len(modelName) < 3 {
+	// Only accept alphanumeric and dashes
+	modelName := string(
+		regexp.MustCompile(
+			"[^a-zA-Z0-9 -]+",
+		).ReplaceAll(
+			[]byte(strings.ToLower(r.FormValue("model_name"))),
+			[]byte(""),
+		),
+	)
+
+	if len(modelName) < 3 && len(modelName) > 50 {
 		util.ERROR(w, http.StatusBadRequest, errors.New("Improper model_name"))
 		return
 	}
 
+	if _, err := os.ReadDir(fmt.Sprintf("models/textrecognition/%s", modelName)); err != nil {
+		util.ERROR(w, http.StatusBadRequest, errors.New("model already exists"))
+		return
+	}
+
 	modelFile, modelHeader, err := r.FormFile("model")
-	if err != nil {
+	if strings.Split(modelHeader.Filename, ".")[1] != "safetensors" || err != nil {
 		util.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
 
-	_, _ = modelFile, modelHeader
+	defer modelFile.Close()
+
+	configFile, configHeader, err := r.FormFile("config")
+	if strings.Split(configHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer configFile.Close()
+
+	generationConfigFile, generationConfigHeader, err := r.FormFile("generation_config")
+	if strings.Split(generationConfigHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer generationConfigFile.Close()
+
+	mergesFile, mergesHeader, err := r.FormFile("merges")
+	if strings.Split(mergesHeader.Filename, ".")[1] != "txt" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer mergesFile.Close()
+
+	preprocessorConfigFile, preprocessorConfigHeader, err := r.FormFile("preprocessor_config")
+	if strings.Split(preprocessorConfigHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer preprocessorConfigFile.Close()
+
+	specialTokensMapFile, specialTokensMapHeader, err := r.FormFile("special_tokens_map")
+	if strings.Split(specialTokensMapHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer specialTokensMapFile.Close()
+
+	tokenizerFile, tokenizerHeader, err := r.FormFile("tokenizer")
+	if strings.Split(tokenizerHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer tokenizerFile.Close()
+
+	tokenizerConfigFile, tokenizerConfigHeader, err := r.FormFile("tokenizer_config")
+	if strings.Split(tokenizerConfigHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer tokenizerConfigFile.Close()
+
+	vocabFile, vocabHeader, err := r.FormFile("vocab")
+	if strings.Split(vocabHeader.Filename, ".")[1] != "json" || err != nil {
+		util.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	defer vocabFile.Close()
+
+	files := map[string]multipart.File{
+		"model.safetensors":        modelFile,
+		"config.json":              configFile,
+		"generation_config.json":   generationConfigFile,
+		"merges.txt":               mergesFile,
+		"preprocessor_config.json": preprocessorConfigFile,
+		"special_tokens_map.json":  specialTokensMapFile,
+		"tokenizer.json":           tokenizerFile,
+		"tokenizer_config.json":    tokenizerConfigFile,
+		"vocab.json":               vocabFile,
+	}
+
+	if err := model.AddTrOCRModel(modelName, "textrecognition", files); err != nil {
+		util.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
 
 	util.JSON(w, http.StatusNoContent, nil)
 }
--- src/domain/model/model.go
+++ src/domain/model/model.go
@@ -6,9 +6,11 @@ import (
 	"io"
 	"mime/multipart"
 	"os"
+	"sync"
 
 	"github.com/erlendromo/forsete-atr/src/domain/model/trocrmodel"
 	"github.com/erlendromo/forsete-atr/src/domain/model/yolomodel"
+	"github.com/erlendromo/forsete-atr/src/util"
 )
 
 var models map[string]Model
@@ -50,12 +52,31 @@ func InitModels() error {
 			modelDirName := modelDir.Name()
 
 			if entryDirName == "linesegmentation" || entryDirName == "regionsegmentation" {
+				entries, err := os.ReadDir(modelDirName)
+				if err != nil {
+					return err
+				}
+
+				found := false
+				for _, entry := range entries {
+					if entry.Name() == "model.pt" {
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					return errors.New("Yolo-model missing 'model.pt' file...")
+				}
+
 				models[modelDirName] = yolomodel.NewYoloModel(
 					modelDirName,
 					fmt.Sprintf("models/%s/%s/model.pt", entryDirName, modelDirName),
 					entryDirName,
 				)
 			} else if entryDirName == "textrecognition" {
+				// TODO add check for files here?
+
 				models[modelDirName] = trocrmodel.NewTrOCRModel(
 					modelDirName,
 					fmt.Sprintf("models/%s/%s", entryDirName, modelDirName),
@@ -79,15 +100,6 @@ func Path(name string) (string, bool) {
 	return model.Path(), found
 }
 
-func Models() []Model {
-	localModels := make([]Model, 0)
-	for _, model := range models {
-		localModels = append(localModels, model)
-	}
-
-	return localModels
-}
-
 func ModelsByType(modelType string) []Model {
 	modelsResponse := make([]Model, 0)
 
@@ -105,14 +117,9 @@ func ModelsByType(modelType string) []Model {
 func AddYoloModel(modelName, modelType string, modelFile multipart.File) error {
 	defer modelFile.Close()
 
-	var modelDirPath string
-	switch modelType {
-	case "regionsegmentation":
-		modelDirPath = fmt.Sprintf("models/regionsegmentation/%s", modelName)
-	case "linesegmentation":
-		modelDirPath = fmt.Sprintf("models/linesegmentation/%s", modelName)
-	default:
-		return errors.New("invalid model type")
+	modelDirPath, err := modelDirPath(modelName, modelType)
+	if err != nil {
+		return err
 	}
 
 	if err := os.MkdirAll(modelDirPath, os.ModeDir); err != nil {
@@ -121,18 +128,74 @@ func AddYoloModel(modelName, modelType string, modelFile multipart.File) error {
 
 	modelPath := fmt.Sprintf("%s/model.pt", modelDirPath)
 
-	localModelFile, err := os.Create(modelPath)
-	if err != nil {
-		return err
-	}
-
-	defer localModelFile.Close()
-
-	if _, err := io.Copy(localModelFile, modelFile); err != nil {
+	if err := createLocalFile(modelFile, modelPath); err != nil {
 		return err
 	}
 
 	models[modelName] = yolomodel.NewYoloModel(modelName, modelPath, modelType)
 
 	return nil
+}
+
+func AddTrOCRModel(modelName, modelType string, files map[string]multipart.File) error {
+	modelDirPath, err := modelDirPath(modelName, modelType)
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(modelDirPath, os.ModeDir); err != nil {
+		return err
+	}
+
+	var wg *sync.WaitGroup
+
+	for fileName, file := range files {
+		wg.Add(1)
+		go func(fileName string, file multipart.File) {
+			defer file.Close()
+			if err := createLocalFile(file, fmt.Sprintf("%s/%s", modelDirPath, fileName)); err != nil {
+				fmt.Printf("\n%s%s%s\n%v",
+					util.RED,
+					"ERROR",
+					util.RESET,
+					err,
+				)
+			}
+			wg.Done()
+		}(fileName, file)
+	}
+
+	wg.Wait()
+
+	models[modelName] = trocrmodel.NewTrOCRModel(modelName, modelDirPath, modelType)
+
+	return nil
+}
+
+func createLocalFile(file multipart.File, path string) error {
+	localFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer localFile.Close()
+
+	if _, err := io.Copy(localFile, file); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func modelDirPath(modelName, modelType string) (string, error) {
+	switch modelType {
+	case "regionsegmentation":
+		return fmt.Sprintf("models/regionsegmentation/%s", modelName), nil
+	case "linesegmentation":
+		return fmt.Sprintf("models/linesegmentation/%s", modelName), nil
+	case "textrecognition":
+		return fmt.Sprintf("models/textrecognition/%s", modelName), nil
+	default:
+		return "", errors.New("invalid model type")
+	}
 }
