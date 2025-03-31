2093c31cf2ebb966ce80f6fa2d6a7ac101645053
erlend <erlenrom@stud.ntnu.no>
2025-04-01 00:44:26 +0200

    feat: Added check for htrflow availability #31

--- src/api/handler/status/statushandler.go
+++ src/api/handler/status/statushandler.go
@@ -1,7 +1,10 @@
-package handler
+package status
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"os/exec"
 
 	"github.com/erlendromo/forsete-atr/src/util"
 )
@@ -11,6 +14,7 @@ import (
 //	@Summary		Status
 //	@Description	Json-response for Status
 type Status struct {
+	ATR     string `json:"atr"`
 	Version string `json:"version"`
 	Uptime  string `json:"uptime"`
 }
@@ -21,8 +25,14 @@ type Status struct {
 //	@Description	Retrieve status of service
 //	@Tags			Status
 //	@Success		204
+//	@Failure		500	{object}	util.ErrorResponse
 //	@Router			/forsete-atr/v1/status/ [head]
 func HeadStatus(w http.ResponseWriter, r *http.Request) {
+	if err := exec.Command("/bin/bash", "-c", "source /htrflow/venv/bin/activate && htrflow pipeline --help").Run(); err != nil {
+		fmt.Printf("\n%sHTRFLOW ERROR%s\n%s\n", util.RED, util.RESET, err.Error())
+		util.ERROR(w, http.StatusInternalServerError, errors.New(util.INTERNAL_SERVER_ERROR))
+	}
+
 	util.JSON(w, http.StatusNoContent, nil)
 }
 
@@ -35,7 +45,15 @@ func HeadStatus(w http.ResponseWriter, r *http.Request) {
 //	@Success		200	{object}	Status
 //	@Router			/forsete-atr/v1/status/ [get]
 func GetStatus(w http.ResponseWriter, r *http.Request) {
+	atr := "ready"
+
+	if err := exec.Command("/bin/bash", "-c", "source /htrflow/venv/bin/activate && htrflow pipeline --help").Run(); err != nil {
+		fmt.Printf("\n%sHTRFLOW ERROR%s\n%s\n", util.RED, util.RESET, err.Error())
+		atr = "unavailable"
+	}
+
 	util.JSON(w, http.StatusOK, &Status{
+		ATR:     atr,
 		Version: util.VERSION,
 		Uptime:  util.UpTimeInHHMMSS(),
 	})
