c887834de8fe872c3bae69e6579f5236e556e386
erlend <erlenrom@stud.ntnu.no>
2025-04-01 00:50:44 +0200

    feat: Logging environment on launch #17

--- src/cmd/rest/rest.go
+++ src/cmd/rest/rest.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"fmt"
+
 	"github.com/erlendromo/forsete-atr/src/api/router/httprouter"
 	"github.com/erlendromo/forsete-atr/src/config"
 	"github.com/erlendromo/forsete-atr/src/util"
@@ -10,6 +12,15 @@ func StartRestService() {
 	util.StartTimer()
 	config := config.GetConfig()
 	router := httprouter.NewHTTPRouter(config.API_PORT)
+
+	fmt.Printf(
+		"\n%sEnvironment%s\nAPI_PORT: %s\nDEVICE: %s\nTIMEOUT: %s\n\n",
+		util.PURPLE,
+		util.RESET,
+		config.API_PORT,
+		config.DEVICE,
+		config.TIMEOUT,
+	)
 
 	if err := router.Serve(); err != nil {
 		panic(err)
