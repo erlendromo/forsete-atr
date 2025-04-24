50a92a8dd2e7fc45f0daf78950b334ea6dd72c93
erlend <erlenrom@stud.ntnu.no>
2025-04-24 23:05:25 +0200

    feat: Added user domain #54

---
+++ src/business/domain/auth/user/user.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       uuid.UUID `db:"id" json:"id"`
+	Email    string    `db:"email" json:"email,omitempty"`
+	Password string    `db:"password" json:"-"`
+	RoleID   int       `db:"role_id" json:"-"`
+	RoleName string    `db:"role_name" json:"-"`
+}
+
+// If err is nil -> password is correct
+func (u *User) ComparePassword(unhashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(unhashedPassword))
+}
