b344fd0e5a4eb545f2652f73f0ecd5eb7704cce8
erlend <erlenrom@stud.ntnu.no>
2025-04-29 15:27:38 +0200

    feat: Moved and improved user/session domains #57 #58

--- src/business/domain/user/user.go
+++ src/business/domain/user/user.go
@@ -1,16 +1,19 @@
 package user
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID       uuid.UUID `db:"id" json:"id"`
-	Email    string    `db:"email" json:"email,omitempty"`
-	Password string    `db:"password" json:"-"`
-	RoleID   int       `db:"role_id" json:"-"`
-	RoleName string    `db:"role_name" json:"-"`
+	ID        uuid.UUID `db:"id" json:"id"`
+	Email     string    `db:"email" json:"email,omitempty"`
+	Password  string    `db:"password" json:"-"`
+	CreatedAt time.Time `db:"created_at" json:"-"`
+	RoleID    int       `db:"role_id" json:"-"`
+	RoleName  string    `db:"role_name" json:"-"`
 }
 
 // If err is nil -> password is correct
