ef97ffbd53b2e32fce8623c34e0377f76535ba06
erlend <erlenrom@stud.ntnu.no>
2025-04-30 18:28:17 +0200

    feat: Added output-domain and queries #62

--- src/business/domain/output/output.go
+++ src/business/domain/output/output.go
@@ -9,8 +9,11 @@ import (
 type Output struct {
 	ID        uuid.UUID `db:"id" json:"id"`
 	Name      string    `db:"name" json:"name"`
+	Format    string    `db:"format" json:"format"`
 	Path      string    `db:"path" json:"-"`
 	CreatedAt time.Time `db:"created_at" json:"-"`
+	UpdatedAt time.Time `db:"updated_at" json:"-"`
+	DeletedAt time.Time `db:"deleted_at" json:"-"`
 	Confirmed bool      `db:"confirmed" json:"confirmed"`
 	ImageID   uuid.UUID `db:"image_id" json:"image_id"`
 }
---
+++ src/business/usecase/repository/output_repository/output_repository.go
@@ -0,0 +1,133 @@
+package outputrepository
+
+import (
+	"context"
+
+	"github.com/erlendromo/forsete-atr/src/business/domain/output"
+	"github.com/erlendromo/forsete-atr/src/database"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+type OutputRepository struct {
+	db *sqlx.DB
+}
+
+func NewOutputRepository(db *sqlx.DB) *OutputRepository {
+	return &OutputRepository{
+		db: db,
+	}
+}
+
+func (o *OutputRepository) OutputByID(ctx context.Context, id uuid.UUID) (*output.Output, error) {
+	query := `
+		SELECT
+			id,
+			name,
+			format,
+			path,
+			created_at,
+			updated_at,
+			deleted_at,
+			confirmed,
+			image_id
+		FROM
+			"output"
+		WHERE
+			id = $1
+		AND
+			deleted_at IS NULL
+	`
+
+	return database.QueryRowx[output.Output](ctx, o.db, query, id)
+}
+
+func (o *OutputRepository) OutputsByImageID(ctx context.Context, imageID uuid.UUID) ([]*output.Output, error) {
+	query := `
+		SELECT
+			id,
+			name,
+			format,
+			path,
+			created_at,
+			updated_at,
+			deleted_at,
+			confirmed,
+			image_id
+		FROM
+			"output"
+		WHERE
+			image_id = $1
+		AND
+			deleted_at IS NULL
+	`
+
+	return database.Queryx[output.Output](ctx, o.db, query, imageID)
+}
+
+func (o *OutputRepository) RegisterOutput(ctx context.Context, name, format, path string, imageID uuid.UUID) (*output.Output, error) {
+	query := `
+		INSERT INTO
+			"output" (name, format, path, image_id)
+		VALUES
+			($1, $2, $3, $4)
+	`
+
+	return database.QueryRowx[output.Output](ctx, o.db, query, name, format, path, imageID)
+}
+
+func (o *OutputRepository) UpdateOutputByID(ctx context.Context, id uuid.UUID, confirmed bool) (*output.Output, error) {
+	query := `
+		UPDATE
+			"output"
+		SET
+			confirmed = $1,
+			updated_at = now()
+		WHERE
+			id = $2
+		AND
+			deleted_at IS NULL
+		RETURNING
+			id,
+			name,
+			format,
+			path,
+			created_at,
+			updated_at,
+			deleted_at,
+			confirmed,
+			image_id
+	`
+
+	return database.QueryRowx[output.Output](ctx, o.db, query, confirmed, id)
+}
+
+func (o *OutputRepository) DeleteOutputByID(ctx context.Context, id uuid.UUID) (int, error) {
+	query := `
+		UPDATE
+			"output"
+		SET
+			deleted_at = now()
+		WHERE
+			id = $1
+		AND
+            deleted_at IS NULL
+	`
+
+	return database.ExecuteContext(ctx, o.db, query, id)
+}
+
+func (o *OutputRepository) SoftDeleteOutputsByImageID(ctx context.Context, imageID uuid.UUID) (int, error) {
+	query := `
+        UPDATE
+            "output"
+        SET
+            deleted_at = now()
+        WHERE
+            image_id = $1
+        AND
+            deleted_at IS NULL
+    `
+
+	return database.ExecuteContext(ctx, o.db, query, imageID)
+}
