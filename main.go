f7fd8dede4b96593158e896978d34d315b695cfe
erlend <erlenrom@stud.ntnu.no>
2025-05-05 02:00:50 +0200

    fix: Removed old logic blocking application #65

--- main.go
+++ main.go
@@ -2,14 +2,7 @@ package main
 
 import (
 	"github.com/erlendromo/forsete-atr/src/cmd"
-	"github.com/erlendromo/forsete-atr/src/domain/modelstore"
 )
-
-func init() {
-	if err := modelstore.GetModelstore().Initialize(); err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	cmd.StartService()
