4ab1b943fae03658c35b1d0410755a9ce4c42160
erlend <erlenrom@stud.ntnu.no>
2025-05-08 01:54:49 +0200

    feat: Added CTE query for registring users #57

--- src/business/usecase/repository/user_repository/user_repository.go
+++ src/business/usecase/repository/user_repository/user_repository.go
@@ -21,15 +21,30 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 
 func (u *UserRepository) RegisterUser(ctx context.Context, email, hashedPassword string) (*user.User, error) {
 	query := `
+		WITH updated AS (
+		  	UPDATE
+				"user"
+			SET
+				password = $2,
+			    deleted_at = NULL
+			WHERE
+				email = $1
+			AND
+				deleted_at IS NOT NULL
+			RETURNING
+				id
+		)
 		INSERT INTO
 			"user" (email, password)
-		VALUES
-			($1, $2)
-		ON CONFLICT
-			(email)
-		DO UPDATE SET
-			password = EXCLUDED.password,
-			deleted_at = NULL
+		SELECT
+			$1,
+			$2
+		WHERE NOT EXISTS (
+			SELECT
+				1
+			FROM
+				updated
+		)
 		RETURNING
 			id
 	`
